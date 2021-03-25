bc38ead9a58b0239a5582f10cc0570fafee86966
Mathew Chan <mchan6466@gmail.com>
2021-03-26 00:51:56 +0800

    Update build.sh
    Update mac os build for newer log folder system

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 rm -rf build, dist
-python -m eel game2text.py web --windowed --icon "icon.ico" --add-data "ImageLogs/temp.png:ImageLogs"
+python -m eel game2text.py web --windowed --icon "icon.ico" --add-data "logs/images/temp.png:logs/images" --add-data "logs/text:logs/text" --add-data "config.ini:."
